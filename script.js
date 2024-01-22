a5e5bf6a85339ef5b96f7644d6ed93ae70bf1f39
tatomika <gvantsajikhvadze93@gmail.com>
2024-01-23 00:03:14 +0400

    Added carousel, js, and changed some style content.

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-hello
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.querySelectorAll('.mySlides');
+    const dots = document.querySelectorAll('.dot');
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].className += ' active';
+    
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
